4848b138956640293ab7be7a378baed14b9e2f89
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2022-03-21 00:28:08 +0400

    NXBasicStructureAdded

---
+++ apps/ngshop/src/app/pages/product-list/product-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'ngshop-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss']
+})
+export class ProductListComponent implements OnInit {
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  constructor() { }
+
+  // eslint-disable-next-line @angular-eslint/no-empty-lifecycle-method
+  ngOnInit(): void {
+    
+  }
+
+}
