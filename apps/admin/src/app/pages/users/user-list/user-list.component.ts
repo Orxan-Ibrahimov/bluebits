1df42c1535485a225be7e172fe89e688bc420fa1
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2022-03-27 21:25:19 +0400

    UserlistShowedByCountryName

--- apps/admin/src/app/pages/users/user-list/user-list.component.ts
+++ apps/admin/src/app/pages/users/user-list/user-list.component.ts
@@ -2,38 +2,41 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User, UsersService } from '@bluebits/my-users';
 import { ConfirmationService, MessageService } from 'primeng/api';
+import * as countriesLib from 'i18n-iso-countries';
+
+declare const require;
 
 @Component({
   selector: 'admin-user-list',
   templateUrl: './user-list.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class UserListComponent implements OnInit {
-
-  users:User[] = [];
-
-
+  users: User[] = [];
+  countries = [];
 
   constructor(
-    private usersService:UsersService,
-    private confirmationService:ConfirmationService,
-    private messageService:MessageService,
-    private router:Router) { }
-
+    private usersService: UsersService,
+    private confirmationService: ConfirmationService,
+    private messageService: MessageService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this._UserList();
   }
 
-
-  private _UserList(){
-   this.usersService.getUsers().subscribe(data => {
-     this.users = data;
-   });
+  private _UserList() {
+    this.usersService.getUsers().subscribe((data) => {
+      this._getCountries();
+      this.users = data;
+      this.users.map((user) => {
+        user.country = this.countries.find((u) => u.id == user.country)?.name;
+      });
+    });
   }
 
-  deleteUser(userId:string){
+  deleteUser(userId: string) {
     this.confirmationService.confirm({
       message: 'Are you sure that you want to remove this user?',
       header: 'Delete?',
@@ -47,11 +50,23 @@ export class UserListComponent implements OnInit {
           });
           this._UserList();
         });
+      },
+    });
   }
-})
-}
 
-editUser(userId:string){
-  this.router.navigateByUrl(`users/update/${userId}`);
-}
+  private _getCountries() {
+    countriesLib.registerLocale(require('i18n-iso-countries/langs/en.json'));
+    this.countries = Object.entries(
+      countriesLib.getNames('en', { select: 'official' })
+    ).map((res) => {
+      return {
+        id: res[0],
+        name: res[1],
+      };
+    });
+  }
+
+  editUser(userId: string) {
+    this.router.navigateByUrl(`users/update/${userId}`);
+  }
 }
